e9f1bce0e0e101c3ab1730dce457bbaa8b5340fb
will <will@will.com>
2017-08-07 00:23:01 +0800

    Update at 20170807002301 by dev/will

--- demo/src/main/java/com/qianyi/dao/PersonDao.java
+++ demo/src/main/java/com/qianyi/dao/PersonDao.java
@@ -2,11 +2,12 @@ package com.qianyi.dao;
 
 import com.minlia.cloud.dao.BatisDao;
 import com.qianyi.domain.Person;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * Created by will on 8/2/17.
  */
-//@Mapper
+@Mapper
 public interface PersonDao extends BatisDao<Person,Long> {
 
 //    public Person findById(Long id);
